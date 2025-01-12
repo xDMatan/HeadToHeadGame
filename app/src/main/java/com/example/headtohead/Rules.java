1a86b69cbd0354f3f3acb890865c1b329e8d3366
xDMatan <mattanpinhas07@gmail.com>
2025-01-12 08:51:07 +0200

    First Gen

--- app/src/main/java/com/example/headtohead/Rules.java
+++ app/src/main/java/com/example/headtohead/Rules.java
@@ -18,6 +18,7 @@ public class Rules extends AppCompatActivity {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
             return insets;
         });
     }
